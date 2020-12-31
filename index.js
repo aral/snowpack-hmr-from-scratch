5f136b30015cf274e9cc31f1e43ca2fbb1ea41be
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2020-12-31 18:59:38 +0000

    Implement state preservation

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import.meta.hot.accept()
+if (import.meta.hot) {
+  import.meta.hot.accept(({module}) => {
+    // Restore state on the new module.
+    module.setState(import.meta.hot.data)
+  })
+  import.meta.hot.dispose(() => {
+    // Save state before destroying the current module.
+    import.meta.hot.data = { timer }
+    destroy()
+  })
+}
 
 import './index.css'
 import confetti from 'canvas-confetti'
